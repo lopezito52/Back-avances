ab6c253e8d218404be6982bedba2f252c695bf92
Esteban7108 <daviddiava@unisabana.edu.co>
2024-05-23 13:41:55 -0500

    Tweeetzzz

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const jwt = require("jsonwebtoken");
 const morgan = require("morgan");
 const { db } = require("./firebase");
 const { v4: uuidv4 } = require("uuid");
+const admin = require('firebase-admin'); 
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
-let users = [];
 
 const listUserAdmin = [{ email: "admin@admin.com", password: "admin" }];
 let refreshTokens = [];
@@ -18,8 +17,6 @@ let refreshTokens = [];
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-
-console.log("Server running on port 3000");
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
@@ -29,14 +26,31 @@ app.get("/", (req, res) => {
   res.send("Hola");
 });
 
-app.post("/users", async (req, res) => {
+app.get('/users', async (req, res) => {
   try {
-    const user = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password, 
-    };
+    const snapshot = await db.collection('contacts').get();
+    const users = [];
+    snapshot.forEach(doc => {
+      const userData = doc.data();
+      userData.id = doc.id;
+      users.push(userData);
+    });
+    res.json(users);
+  } catch (error) {
+    console.error('Error retrieving users:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post("/users", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  if (!firstName || !lastName || !email || !password) {
+    return res.status(400).send("Missing required fields");
+  }
+
+  try {
+    const user = { firstName, lastName, email, password };
     const userRef = await db.collection("contacts").add(user);
     const userId = userRef.id;
     console.log("User ID:", userId);
@@ -48,14 +62,15 @@ app.post("/users", async (req, res) => {
 });
 
 app.post("/users/login", async (req, res) => {
-  try {
-    const email = req.body.email;
-    const password = req.body.password;
+  const { email, password } = req.body;
 
-    const snapshot = await db
-      .collection("contacts")
-      .where("email", "==", email)
-      .get();
+  if (!email || !password) {
+    return res.status(400).send("Missing email or password");
+  }
+
+  try {
+    const snapshot = await db.collection("contacts").where("email", "==", email).get();
+
     if (snapshot.empty) {
       return res.status(400).send("Cannot find user");
     }
@@ -66,23 +81,16 @@ app.post("/users/login", async (req, res) => {
       user.id = doc.id;
     });
 
-    console.log("User found:", user);
-    console.log("Entered password:", password);
-    console.log("Stored password:", user.password);
-
     if (password === user.password) {
       const accessToken = generateAccessToken({ email: user.email });
-      const refreshToken = jwt.sign(
-        { email: user.email },
-        process.env.REFRESH_TOKEN_SECRET
-      );
+      const refreshToken = jwt.sign({ email: user.email }, process.env.REFRESH_TOKEN_SECRET);
       refreshTokens.push(refreshToken);
       res.cookie("session", refreshToken, {
         httpOnly: true,
         secure: true,
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
-      res.json({ accessToken, refreshToken });
+      res.json({ accessToken, refreshToken, userId: user.id, userName: `${user.firstName} ${user.lastName}` });
     } else {
       res.status(403).send("Not allowed");
     }
@@ -98,10 +106,7 @@ app.get("/edit-contact/:id", async (req, res) => {
     if (!doc.exists) {
       return res.status(404).send("Contact not found");
     }
-    const contact = {
-      id: doc.id,
-      ...doc.data(),
-    };
+    const contact = { id: doc.id, ...doc.data() };
     res.json(contact);
   } catch (error) {
     console.error("Error retrieving contact:", error);
@@ -109,7 +114,7 @@ app.get("/edit-contact/:id", async (req, res) => {
   }
 });
 
-app.get("/delete-contact/:id", authenticateToken, async (req, res) => {
+app.delete("/delete-contact/:id", authenticateToken, async (req, res) => {
   try {
     await db.collection("contacts").doc(req.params.id).delete();
     res.send("Contact deleted");
@@ -140,22 +145,15 @@ app.delete("/logout", (req, res) => {
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
-  const user = listUserAdmin.find(
-    (user) => user.email === email && user.password === password
-  );
+  const user = listUserAdmin.find(user => user.email === email && user.password === password);
 
   if (!user) {
-    return res
-      .status(401)
-      .json({ message: "Correo electrónico o contraseña incorrectos" });
+    return res.status(401).json({ message: "Correo electrónico o contraseña incorrectos" });
   }
 
   try {
     const accessToken = generateAccessToken({ email: user.email });
-    const refreshToken = jwt.sign(
-      { email: user.email },
-      process.env.REFRESH_TOKEN_SECRET
-    );
+    const refreshToken = jwt.sign({ email: user.email }, process.env.REFRESH_TOKEN_SECRET);
     refreshTokens.push(refreshToken);
     res.cookie("session", refreshToken, {
       httpOnly: true,
@@ -168,6 +166,87 @@ app.post("/login", (req, res) => {
     res.status(500).json({ message: "Error interno del servidor" });
   }
 });
+
+app.post("/tweets", async (req, res) => {
+  const { userId, userName, tweet } = req.body;
+
+  console.log('Request received with body:', req.body);
+
+  if (!userId || !userName || !tweet) {
+    console.log('Missing required fields:', { userId, userName, tweet });
+    return res.status(400).send('Missing required fields');
+  }
+
+  try {
+    const userRef = db.collection('contacts').doc(userId);
+    const doc = await userRef.get();
+
+    if (!doc.exists) {
+      console.log('User not found:', userId);
+      return res.status(404).send('User not found');
+    }
+
+    const tweetData = {
+      userId,
+      userName,
+      tweet,
+      timestamp: admin.firestore.FieldValue.serverTimestamp()
+    };
+    
+    const tweetRef = await db.collection('tweets').add(tweetData);
+    
+    await userRef.update({
+      tweets: admin.firestore.FieldValue.arrayUnion({
+        tweetId: tweetRef.id,
+        tweet: tweet,
+        timestamp: tweetData.timestamp
+      })
+    });
+    
+
+    res.status(201).send('Tweet added successfully');
+  } catch (error) {
+    console.error('Error posting tweet:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/tweets', async (req, res) => {
+  try {
+    const snapshot = await db.collection('tweets').orderBy('timestamp', 'desc').get();
+    const tweets = [];
+    snapshot.forEach(doc => {
+      const tweetData = doc.data();
+      tweetData.id = doc.id;
+      tweets.push(tweetData);
+    });
+    res.json(tweets);
+  } catch (error) {
+    console.error('Error retrieving tweets:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get("/tweets/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const snapshot = await db.collection('tweets').where('userId', '==', userId).get();
+    const tweets = [];
+
+    snapshot.forEach(doc => {
+      const tweetData = doc.data();
+      tweetData.id = doc.id;
+      tweets.push(tweetData);
+    });
+
+    res.json(tweets);
+  } catch (error) {
+    console.error('Error retrieving user tweets:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 
 function generateAccessToken(user) {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
