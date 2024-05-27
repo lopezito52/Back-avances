0a71b0d6e338af8b4131ce7c4d1cd9925bb3c86d
lopezito52 <samuellohu@unisabana.edu.co>
2024-05-26 22:10:10 -0500

    ok

--- firebase.js
+++ firebase.js
@@ -1,14 +1,31 @@
 require('dotenv').config();
 
-const {initializeApp, applicationDefault} = require('firebase-admin/app')
-const {getFirestore} = require('firebase-admin/firestore')
+const fs = require('fs');
+const path = require('path');
+const { initializeApp, applicationDefault } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
+// Verifica que la variable de entorno esté definida
+if (!process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
+  throw new Error('La variable de entorno GOOGLE_APPLICATION_CREDENTIALS_JSON no está definida');
+}
+
+// Ruta temporal para guardar las credenciales
+const tempCredentialsPath = path.join('/tmp', 'firebase.json');
+
+// Escribir el contenido de la variable de entorno en el archivo temporal
+fs.writeFileSync(tempCredentialsPath, process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+
+// Configurar la variable de entorno GOOGLE_APPLICATION_CREDENTIALS para que apunte al archivo temporal
+process.env.GOOGLE_APPLICATION_CREDENTIALS = tempCredentialsPath;
+
+// Inicializar Firebase Admin SDK
 initializeApp({
     credential: applicationDefault(),
 });
 
 const db = getFirestore();
 
-module.exports={
+module.exports = {
     db,
-}
+};
